f7a33efa28fc497728639dfafa62505de5057ab0
Jeff Bahns <jeffbahns@gmail.com>
2015-01-23 18:35:52 -0800

    complete

--- stutterprimes.java
+++ stutterprimes.java
@@ -1,29 +1,30 @@
 
 public class stutterprimes{
 	
+	// methods are all executed from main
 	public static void main(String args[]){
-		//computePrimes(30);
-		//isPrime(4);
-		printStutter(13);
+		computePrimes(47);
+		isPrime(47);
+		printStutter(47);
 	}
 	
+	// computes/prints every prime between 2 and n
 	public static void computePrimes(int num){
+		System.out.println("Primes between 2 and " + String.valueOf(num));
 		for (int p = 2; p <= num; p++){
-			System.out.println("-----------------");
-			System.out.println(p);
 			boolean decision = true;
 			for (int i = 2; i <= Math.sqrt(p); i++){
 				if (p % i == 0)
 					decision = false;
 			}
 			if (decision)
-				System.out.println("PRIME!");
-			else
-				System.out.println("NOT PRIME!");
+				System.out.println(p);
 		}
 	}
 
+	// decides whether or not number is prime
 	public static void isPrime(int num){
+		System.out.println("Is " + String.valueOf(num) + " prime or not?");
 		boolean decision = true;
 		for (int i = 2; i <= Math.sqrt(num); i++){
 			if (num % i == 0)
@@ -35,13 +36,17 @@ public class stutterprimes{
 			System.out.println("NOT PRIME!");
 	}
 
+	// prints stutter prime of a number
 	public static void printStutter(int num){
-		int lim = 1000000;
-		String jeff = "";
-		while (lim >=1){
-			num = num - lim;
-			System.out.println(jeff);
-			lim = lim / 10;
+		int length = (int) Math.log10(num);
+		int mask = (int) Math.pow(10, length);
+		String outp = "";
+		while (mask >=1){
+			int place = num / mask;
+			num = num - (place * mask);
+			outp += String.valueOf(place) + String.valueOf(place);
+			mask /= 10;
 		}
+		System.out.println(outp);
 	}
 }
