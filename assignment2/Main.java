49e818521c7c3bff4ded151cc1b0a9bb8e8f04bc
Jeff Bahns <jeffbahns@gmail.com>
2015-02-02 20:23:41 -0800

    created main driver

---
+++ assignment2/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main{
+
+	public static void main(String[]args){
+
+    	// creates scanner object, to take input
+    	Scanner s = new Scanner(System.in);
+
+    	// these are a series of print statements asking for input,
+    	// and scanner reads that store the user input
+    	System.out.println("Enter Country Code:");
+    	int cc = s.nextInt();
+		System.out.println("Enter Area Code:");
+		int ac = s.nextInt();
+		System.out.println("Enter Phone Number:");
+		int pnum = s.nextInt();
+		System.out.println("Enter Type (H, C, or B):");
+		String typ = s.next();
+		char type = typ.charAt(0);
+		System.out.println("Enter Year:");
+		int yr = s.nextInt();
+	
+		// once all necessary input is collected, it creates an object with given traits
+		// and preforms different methods of that object
+		PhoneNumber newPhone = new PhoneNumber(cc, ac, pnum, type, yr);
+		newPhone.printNumber();
+		newPhone.printPhoneNumberStats();
+		newPhone.doubleDigits();
+		
+    }
+}
