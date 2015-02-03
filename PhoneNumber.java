8c2b51d20407a245a7b487a048f6800ce306f046
Jeff Bahns <jeffbahns@gmail.com>
2015-02-02 16:25:31 -0800

    fixed spacing issues

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -146,22 +146,22 @@ public class PhoneNumber{
     	// and scanner reads that store the user input
     	System.out.println("Enter Country Code:");
     	int cc = s.nextInt();
-		System.out.println("Enter Area Code:");
-		int ac = s.nextInt();
-		System.out.println("Enter Phone Number:");
-		int pnum = s.nextInt();
-		System.out.println("Enter Type (H, C, or B):");
-		String typ = s.next();
-		char type = typ.charAt(0);
-		System.out.println("Enter Year:");
-		int yr = s.nextInt();
+	System.out.println("Enter Area Code:");
+	int ac = s.nextInt();
+	System.out.println("Enter Phone Number:");
+	int pnum = s.nextInt();
+	System.out.println("Enter Type (H, C, or B):");
+	String typ = s.next();
+	char type = typ.charAt(0);
+	System.out.println("Enter Year:");
+	int yr = s.nextInt();
 	
-		// once all necessary input is collected, it creates an object with given traits
-		// and preforms different methods of that object
-		PhoneNumber newPhone = new PhoneNumber(cc, ac, pnum, type, yr);
-		newPhone.printNumber();
-		newPhone.printPhoneNumberStats();
-		newPhone.doubleDigits();
+	// once all necessary input is collected, it creates an object with given traits
+	// and preforms different methods of that object
+	PhoneNumber newPhone = new PhoneNumber(cc, ac, pnum, type, yr);
+	newPhone.printNumber();
+	newPhone.printPhoneNumberStats();
+	newPhone.doubleDigits();
 		
     }
 }
